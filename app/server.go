1daf7fbdfd5c49b16fc78bf42b0797e9f2d7375b
mohamed.BENTAHER <mohamed.bentaher@innovx.ma>
2024-01-12 21:58:37 +0100

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -18,9 +18,23 @@ func main() {
 		os.Exit(1)
 	}
 	
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	var request []byte 
+	conn.Read(request)
+
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Something went wrong while sending response")
+		os.Exit(1)
+	}
+
 }
